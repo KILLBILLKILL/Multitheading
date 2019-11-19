a18d0f228da9ffbab3757a7f4c9c43522fb331ca
KILLBILLKILL <1043219841@qq.com>
2019-11-19 22:54:18 +0800

    okk

---
+++ src/three/T.java
@@ -0,0 +1,19 @@
+package three;
+
+public class T {
+    private int i=0;
+    private static int anInt=0;
+    public static void run(){}
+    public static void run2()
+    {
+    }
+    public static void main(String[] args) {
+        int i=0;
+        i++;
+        //1.获取i的值
+        //2.修改i的值
+        //3.保存i的值
+        Object o=new Object();
+
+    }
+}
